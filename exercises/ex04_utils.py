51e53d566bbbb0d4db82d2f8b3dd1db3655629a4
Your Name <you@example.com>
2022-09-22 17:27:03 -0400

    ex04 update

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,7 +2,9 @@
 
 __author__ = "730480669"
 
+
 def all(numbers: list[int], digit: int) -> bool:
+    """This function determines if a digit is in a list."""
     i: int = 0
     while i < len(numbers):
         if numbers[i] == digit:
@@ -14,6 +16,7 @@ def all(numbers: list[int], digit: int) -> bool:
 
 
 def max(input: list[int]) -> int:
+    """This function determines the maximum valie in a list."""
     i: int = 0
     maximum = input[0]
     if len(input) == 0:
@@ -27,6 +30,7 @@ def max(input: list[int]) -> int:
 
 
 def is_equal(list_one: list[int], list_two: list[int]) -> bool:
+    """This function determines if one list is equal to another list."""
     i: int = 0
     valid: bool = False
     while i < len(list_one):
