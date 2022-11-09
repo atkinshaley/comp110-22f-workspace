9919d5d8b9d44ecfcd932eae43403e3f1c80899f
Your Name <you@example.com>
2022-11-08 23:43:58 -0500

    new updates

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -1,5 +1,73 @@
 """Dictionary related utility functions."""
 
-__author__ = ""
+__author__ = "730480669"
 
-# Define your functions below
+
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a CSV into a 'table'."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close()
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+    return result
+
+
+def head(constant: dict[str, list[str]], N: int) -> dict[str, list[str]]:
+    """Produces a new table with only N rows of data."""
+    i: int = 0
+    new_dict: dict[str, list[str]] = {}
+    for column in constant:
+        new_values: list[str] = []
+        while i < N: 
+            new_values.append(column)
+            i += 1
+        new_dict = [constant[column], new_values]
+    return new_dict
+
+
+def select(constant: dict[str, list[str]], columns: list[str]) -> dict[str, list[str]]:
+    """Returns a subset of the original dictionary."""
+    new_dict: dict[str, list[str]] = {}
+    for x in constant:
+        new_dict[x] = constant[columns]
+    return new_dict
+
+
+def concat(constant: dict[str, list[str]], constant_two: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Creates a new column-based table withtwocolumn-based tables combined."""
+    table: dict[str, list[str]] = {}
+    return table
+
+
+def count(counter: list[str]) -> dict[str, int]:
+    """Produces a dictionary and counts the times a value appears in the given list."""
+    new_dict: dict[counter: list[str], int] = {}
+    for item in counter:
+        if item in counter:
+            new_dict[counter] += 1
+        else:
+            new_dict[counter] = 1
+    return new_dict
--- exercises/ex09/model.py
+++ exercises/ex09/model.py
@@ -6,7 +6,7 @@ from exercises.ex09 import constants
 from math import sin, cos, pi
 
 
-__author__ = ""  # TODO
+__author__ = "730480669"
 
 
 class Point:
@@ -30,7 +30,7 @@ class Cell:
     """An individual subject in the simulation."""
     location: Point
     direction: Point
-    sickness: int = 0
+    sickness: int = constants.VULNERABLE
 
     def __init__(self, location: Point, direction: Point):
         """Construct a cell with its location and direction."""
@@ -41,10 +41,36 @@ class Cell:
     # Its purpose is to reassign the object's location attribute
     # the result of adding the self object's location with its
     # direction. Hint: Look at the add method.
+    def tick(self) -> None:
+        self.location = self.location.add(self.direction)
         
     def color(self) -> str:
         """Return the color representation of a cell."""
         return "black"
+
+    def contract_disease(self) -> None:
+        """Assigns INFECTED state to Cell."""
+        Cell.sickness = constants.INFECTED
+
+    def is_vulnurable(self) -> bool:
+        """Tests non-sickness of Cell."""
+        if not isinstance(Cell.sickness, constants.VULNERABLE):
+            return False
+
+    def is_infected(self) -> bool:
+        """Tests sickness of Cell."""
+        if not isinstance(Cell.sickness, constants.INFECTED):
+            return False
+
+    def color(self) -> str:
+        """Determines color of Cell based on sickness."""
+        if Cell.is_vulnerable:
+            return "gray"
+        if Cell.is_infected:
+            return "pink"
+
+        
+
 
 
 class Model:
@@ -52,28 +78,47 @@ class Model:
 
     population: list[Cell]
     time: int = 0
+    infection_count: int
 
     def __init__(self, cells: int, speed: float):
         """Initialize the cells with random locations and directions."""
         self.population = []
+        for _ in range(cells):
+            start_location: Point = self.random_location()
+            start_direction: Point = self.random_direction(speed)
+            cell: Cell = Cell(start_location, start_direction)
+            self.population.append(cell)
+        if self.infection_count >= 0:
+            raise ValueError("Initial cell infection population must be greater than zero!")
+        if self.infection_count > len(self.population):
+            raise ValueError("Infected cell count cannot exceed cell population.")
+
     
     def tick(self) -> None:
         """Update the state of the simulation by one time step."""
         self.time += 1
+        for cell in self.population:
+            cell.tick()
+            self.enforce_bounds(cell)
 
     def random_location(self) -> Point:
         """Generate a random location."""
-        # TODO
-        return Point(0.0, 0.0)
+        start_x: float = random() * constants.BOUNDS_WIDTH - constants.MAX_X
+        start_y: float = random() * constants.BOUNDS_HEIGHT - constants.MAX_Y
+        return Point(start_x, start_y)
 
     def random_direction(self, speed: float) -> Point:
         """Generate a 'point' used as a directional vector."""
-        # TODO
-        return Point(0.0, 0.0)
+        random_angle: float = 2.0* pi * random()
+        direction_x: float = cos(random_angle) * speed
+        direction_y: float = sin(random_angle) * speed
+        return Point(direction_x, direction_y)
 
     def enforce_bounds(self, cell: Cell) -> None:
         """Cause a cell to 'bounce' if it goes out of bounds."""
-        ...
+        if cell.location.x > constants.MAX_X:
+            cell.location.x = constants.MAX_X
+            cell.direction.x *= -1.0
 
     def is_complete(self) -> bool:
         """Method to indicate when the simulation is complete."""
